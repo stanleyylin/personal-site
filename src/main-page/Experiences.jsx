0722e9e97413fa4de9affe22cacae6ab94b4cded
stanleyylin <stanleylin82@gmail.com>
2024-02-07 23:21:19 -0500

    edited html/body background and updated experiences

--- src/main-page/Experiences.jsx
+++ src/main-page/Experiences.jsx
@@ -5,8 +5,8 @@ import moon from '../res/moon.png';
 import Box from '../components/Box';
 
 const Experiences = () => {
-  const points1 = ["> Engineered responsive web app to allow business stakeholders to access and analyze critical sales, inventory, and customer data.", "> Designed and implemented frontend design using React.js, Typescript, and styled components.", "> Demonstrated teamwork skills by actively participating in cross-functional collaboration, leading to a 45% reduction in development cycle time and improved overall project efficiency."];
-  const points2 = ["> Developing a cross-platform real-estate application for a real estate agent, providing optimized property searching and appointment booking for over 100+ clients.", "> Utilizing React, MongoDB, ExpressJS, NodeJS, Styled Components, and the Realtor.ca API to implement user-centric design principles and features and to seamlessly integrate real estate data."];
+  const points1 = ["> Engineering robust web interfaces for Deep Trekker's Remote Operated Vehicle controller app, using TypeScript, Next.js, and Redux, enhancing user experiences for thousands of global customers.", "> Developing components with Storybook and facilitating unit testing to identify and fix 10+ controller bugs.", "> Integrated MySQL database and Express.js backend for vehicle analytics and dive logs."];
+  const points2 = ["> Engineered responsive web app to allow business stakeholders to access and analyze critical sales, inventory, and customer data.", "> Designed and implemented frontend design using React.js, Typescript, and styled components.", "> Demonstrated teamwork skills by actively participating in cross-functional collaboration, leading to a 45% reduction in development cycle time and improved overall project efficiency."];
   const points3 = ["> Collaborated with a team of Stanford AI students led by Dr. Andrew Ng to create a cutting-edge, python computer science curriculum for high school students.", "> Led committee meetings and developed modules both in Google Colaboratory (Tensorflow) and in the web app.", "> Technologies used: Python, Tensorflow, Machine Learning."];
   const points4 = ["> Organized campus-wide events including club socials, BOT/EOT events, speaker events, games nights, and co-op panels with turnout rates of over 400+ students. ", "> Participated in team meetings, delegated tasks, and organized timelines.", "> Creating designs for posts @uwaterloodsc on instagram with over 2200+ followers."];
   return (
@@ -20,8 +20,8 @@ const Experiences = () => {
       </div>
 
       <div className='experiences-grid'>
-        <Box title='bld.ai' info='Software Engineer Intern | May — August 2023' points={points1}/>
-        <Box title='Homelife New World Realty Inc, Lisa Gao' info='Software Developer (Freelance) | July 2023 - Present' points={points2}/>
+        <Box title='Deep Trekker' info='Web Developer Intern | January 2024 — Present' points={points1}/>
+        <Box title='bld.ai' info='Software Engineer Intern | May — August 2023' points={points2}/>
         <Box title='Student Advisory Council Member' info='Kira Learning | June - August 2021 & 2022' points={points3}/>
         <Box title='Logistics Coordinator, Graphic Designer' info='UWaterloo Data Science Club | DECEMBER—PRESENT 2023' points={points4}/>
 
